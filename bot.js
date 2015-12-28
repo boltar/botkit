c3b3a55df60cdcb38adfa1b6a8a77ea8d0d70f61
boltar <tony.cha@gmail.com>
2015-12-28 05:03:38 +0000

    keep track of jeopardy stats

--- bot.js
+++ bot.js
@@ -69,6 +69,7 @@ var os = require('os');
 
 var controller = Botkit.slackbot({
   debug: false,
+  json_file_store: 'storage'
 });
 
 var bot = controller.spawn(
@@ -77,107 +78,95 @@ var bot = controller.spawn(
   }
 ).startRTM();
 
-
-controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
-
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'robot_face',
-  },function(err,res) {
-    if (err) {
-      bot.botkit.log("Failed to add emoji reaction :(",err);
-    }
-  });
-
-
-  controller.storage.users.get(message.user,function(err,user) {
-    if (user && user.name) {
-      bot.reply(message,"Hello " + user.name+"!!");
-    } else {
-      bot.reply(message,"Hello.");
-    }
-  });
-})
-
-controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot,message) {
-  var matches = message.text.match(/call me (.*)/i);
+controller.hears(['That is correct, (.+)\.'],'direct_message,ambient',function(bot,message) {
+  console.log("heard correct")
+  var matches = message.text.match(/That is correct, (.+)\./);
+  if (matches == null) {
+    return;
+  }
   var name = matches[1];
-  controller.storage.users.get(message.user,function(err,user) {
+  controller.storage.users.get(name,function(err,user) {
     if (!user) {
       user = {
-        id: message.user,
-      }
-    }
-    user.name = name;
-    controller.storage.users.save(user,function(err,id) {
-      bot.reply(message,"Got it. I will call you " + user.name + " from now on.");
-    })
-  })
-});
-
-controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot,message) {
-
-  controller.storage.users.get(message.user,function(err,user) {
-    if (user && user.name) {
-      bot.reply(message,"Your name is " + user.name);
+        id: name,
+        correct: 1,
+        incorrect: 0
+      } 
+      controller.storage.users.save(user, function(err,id) {
+      })
     } else {
-      bot.reply(message,"I don't know yet!");
+      user.correct = user.correct + 1
+      controller.storage.users.save(user, function(err,id) {
+      })
     }
   })
-});
+})
 
-
-controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(bot,message) {
-
-  bot.startConversation(message,function(err,convo) {
-    convo.ask("Are you sure you want me to shutdown?",[
-      {
-        pattern: bot.utterances.yes,
-        callback: function(response,convo) {
-          convo.say("Bye!");
-          convo.next();
-          setTimeout(function() {
-            process.exit();
-          },3000);
-        }
-      },
-      {
-        pattern: bot.utterances.no,
-        default:true,
-        callback: function(response,convo) {
-          convo.say("*Phew!*");
-          convo.next();
-        }
-      }
-    ])
+controller.hears(['That is incorrect, (.+)\.'],'direct_message,ambient',function(bot,message) {
+  console.log("heard incorrect")
+  var matches = message.text.match(/That is incorrect, (.+)\./);
+  if (matches == null) {
+    return;
+  }
+  var name = matches[1];
+  controller.storage.users.get(name,function(err,user) {
+    if (!user) {
+      user = {
+        id: name,
+        correct: 0,
+        incorrect: 1
+      } 
+      controller.storage.users.save(user, function(err,id) {
+      })
+    } else {
+      user.incorrect = user.incorrect + 1
+      controller.storage.users.save(user, function(err,id) {
+      })
+    }
   })
 })
 
-
-controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot,message) {
-
-  var hostname = os.hostname();
-  var uptime = formatUptime(process.uptime());
-
-  bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name +'>. I have been running for ' + uptime + ' on ' + hostname + ".");
-
+controller.hears(['stats (.+)'],'direct_message,direct_mention,mention,ambient',function(bot,message) {
+  console.log("heard stats")
+  
+  var matches = message.text.match(/stats (.+)/);
+  if (matches == null) {
+    return;
+  }
+  var name = matches[1];
+  controller.storage.users.get(name,function(err,user) {
+    if (!user) {
+      bot.reply(message, name + " hasn't answered any questions yet!");
+    } else {
+      var ratio
+      if (user.incorrect + user.correct > 0)
+      {
+        ratio = "(" + (user.correct * 100 / (user.correct + user.incorrect)).toFixed(2) + "% correct)"
+      } else {
+        ratio = ""
+      }
+      bot.reply(message, name + " has answered " + user.correct + " correctly, and " + user.incorrect + " incorrectly. " + ratio );
+    }
+  })
 })
 
-function formatUptime(uptime) {
-  var unit = 'second';
-  if (uptime > 60) {
-    uptime = uptime / 60;
-    unit = 'minute';
+controller.hears(['reset (.+)'],'direct_message,direct_mention,mention,ambient',function(bot,message) {
+  console.log("heard reset")
+  
+  var matches = message.text.match(/reset (.+)/);
+  if (matches == null) {
+    return;
   }
-  if (uptime > 60) {
-    uptime = uptime / 60;
-    unit = 'hour';
-  }
-  if (uptime != 1) {
-    unit = unit +'s';
-  }
-
-  uptime = uptime + ' ' + unit;
-  return uptime;
-}
+  var name = matches[1];
+  controller.storage.users.get(name,function(err,user) {
+    if (!user) {
+      bot.reply(message, name + " doesn't have any stats to reset!")
+    } else {
+      user.incorrect = 0;
+      user.correct = 0;
+      controller.storage.users.save(user, function(err,id) {
+      })
+    }
+    bot.reply(message, "Ok, I'll reset " + name + "'s stats.")
+  })
+})
